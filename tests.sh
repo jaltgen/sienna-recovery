203c430fdf2ef5b5583af0ad46d5aa6005078689
Jannik Altgen <jannik.altgen@gmail.com>
2023-12-28 11:25:41 +0100

    renamed it again

--- tests.sh
+++ tests.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
-. /root/sienna/config.sh 
-$(chown $SIENNA_USER:$SIENNA_GROUP /root/sienna/demo/config.xml)
+# ./tests.sh -[rplb]
+# -r : Running conf removed
+# -p : Pre boot backup removed
+# -l : Last flush backup removed
+# -b : golden Backup removed
+
+. /home/ubuntu/config-saver/config.sh
+OPTIONS='rplb'
+
+while getopts $OPTIONS opt; do
+    case $opt in
+        r)
+            rm $FULL_CONF_PATH
+            ;;
+        p)
+            rm $PRE_BACKUP_PATH
+            ;;
+        l)
+            rm $LAST_BACKUP_PATH
+            ;;
+        b)
+            rm $FULL_BACKUP_PATH
+            ;;
+    esac
+done
